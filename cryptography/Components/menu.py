6174096e086ee6ba9e1e1af8b1f1d8065323e1e0
Anasterrar <anas.terrar@gmail.com>
2025-12-18 10:27:25 +0100

    op

--- cryptography/Components/menu.py
+++ cryptography/Components/menu.py
@@ -1,11 +1,11 @@
-from Components import header
+from Components.header import header
 from Components.text_selection import text_selection
 
 def menu():
     data = text_selection("text")
     while True:
         options = [f"1. {data["menu_caesar"]}", f"2. {data["menu_rot"]}", f"3. {data["menu_Caesar_poly"]}", f"4. {data["menu_vigenere"]}", f"5. {data["menu_Caesar_affine"]}", f"6. {data["menu_setting"]}"]
-        header.header("app_title", None)
+        header("app_title", None)
         for opt in options:
             print(opt)
         answer = input(data["input_menu"])
