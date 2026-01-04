84a41c13ebe623a440e848a46587b600ea7b7b2d
Anasterrar <anas.terrar@gmail.com>
2026-01-05 00:08:46 +0100

    better file creation
    Allow user to select what information put in the file
    (in progresse)

---
+++ cryptography/Components/file_options.py
@@ -0,0 +1,55 @@
+import os
+import msvcrt
+from colorama import Fore, Style, init
+init(autoreset=True)
+
+def file_options():
+    options = [
+    {"label": "Header", "enabled": False},
+    {"label": "Methode", "enabled": False},
+    {"label": "Texte original", "enabled": False},
+    {"label": "Clé", "enabled": False},
+    {"label": "Texte codé", "enabled": True},
+    {"label": "Date et heure", "enabled": False},
+    {"label": "Valider", "enabled": False},
+    ]
+    selected = 0
+    while True:
+        os.system("cls")
+        print("do you want save the file ?")
+        
+        for i, option in enumerate(options):
+            cursor = ">" if i == selected else ""
+            check = " ✅" if option["enabled"] else ""
+            print(f"{cursor} {option['label']}{check}")
+
+        print(Fore.YELLOW + Style.BRIGHT + f"{options[selected]["label"]} ?")
+                
+        key = msvcrt.getch()
+
+        if key == b'\xe0':
+            key2 = msvcrt.getch()
+                
+            if key2 == b'H':
+                selected = (selected - 1) % len(options)
+                print(selected)
+            elif key2 == b'P':
+                    selected = (selected + 1) % len(options)
+
+        elif key == b'\r':
+            if selected < len(options) - 1:
+                options[selected]["enabled"] = not options[selected]["enabled"]
+            
+            if selected == len(options) - 1:
+                print(options)
+                break
+                 
+        elif key == b'\x1b':
+            print(options)
+            break
+
+def main():
+    file_options()
+
+
+main()
