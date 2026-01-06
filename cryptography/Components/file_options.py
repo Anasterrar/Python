6e51ec00f932ed3078a3cdd4007c49caa0fdcfa6
Anasterrar <anas.terrar@gmail.com>
2026-01-06 18:12:24 +0100

    Element choice in saved file

--- cryptography/Components/file_options.py
+++ cryptography/Components/file_options.py
@@ -1,9 +1,12 @@
+import os
+import json
 import msvcrt
 from colorama import Fore, Style, init
-from Components.header import header
-from Components.error_message import error_message
 init(autoreset=True)
 
+from Components.header import header
+from Components.text_selection import text_selection
+from Components.error_message import error_message
 
 def error_options(options):
     a = 0
@@ -14,8 +17,41 @@ def error_options(options):
         return True
     else:
         return False
+    
+def save_options(options):
+    path_file = "config/last_save.json"
+    if os.path.isfile(path_file):
+        with open(path_file, "r", encoding="utf-8") as f:
+            try:
+                last = json.load(f)
+            except json.JSONDecodeError:
+                last = {}
+    else:
+        last = {}
+    last["options"] = options
+    with open(path_file, "w", encoding="utf-8") as f:
+        json.dump(last, f, indent=4, ensure_ascii=False)
 
+def previous_options():
+    path_file = "config/last_save.json"
+    if os.path.isfile(path_file):
+        with open(path_file, "r", encoding="utf-8") as f:
+            previous = json.load(f)
+            if len(previous["options"]) > 0:
+                return previous["options"]
+    else :
+        return None
+
+def previous_list(previous):
+    previous_line = []
+    for line in previous:
+        if line["enabled"] == True:
+            previous_line.append(line["label"])
+    return previous_line
+    
+    
 def file_options():
+    data = text_selection("text")
     options = [
     {"label": "Header", "enabled": False},
     {"label": "Methode", "enabled": False},
@@ -25,6 +61,7 @@ def file_options():
     {"label": "Date et heure", "enabled": False},
     {"label": "Valider", "enabled": False},
     ]
+    previous_choice = previous_options()
     selected = 0
     error = False
     while True:
@@ -32,7 +69,11 @@ def file_options():
 
         if error == True:
             error_message(["error_invalid_selection"])
-        
+
+        print(Fore.YELLOW + Style.BRIGHT + data["input_choice"])
+        print(data["input_previous_choice"] + "\n" + str(previous_list(previous_choice)))
+        print(Fore.MAGENTA + "───────────────────────────────")
+
         for i, option in enumerate(options):
             cursor = ">" if i == selected else ""
             check = " ✅" if option["enabled"] else ""
@@ -41,6 +82,9 @@ def file_options():
         print(Fore.YELLOW + Style.BRIGHT + f"{options[selected]["label"]} ?")
                 
         key = msvcrt.getch()
+
+        if key == b's':
+            return previous_choice
 
         if key == b'\xe0':
             key2 = msvcrt.getch()
@@ -60,6 +104,7 @@ def file_options():
                     error = True
                     continue
                 else:
+                    save_options(options)
                     return options
                  
         elif key == b'\x1b':
