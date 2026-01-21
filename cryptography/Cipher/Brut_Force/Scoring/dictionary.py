4a71629d48416855063c4a831fb70a397dcac2c6
Anasterrar <anas.terrar@gmail.com>
2026-01-21 03:35:48 +0100

    Opti

--- cryptography/Cipher/Brut_Force/Scoring/dictionary.py
+++
@@ -1,7 +0,0 @@
-def load_dictionaries():
-    return {
-        "French": set(open("francais.txt", encoding="utf-8").read().lower().split()),
-        "English": set(open("english.txt", encoding="utf-8").read().lower().split())
-    }
-
-DICTIONARIES = load_dictionaries()
